8317057b1bb0906bd5f70c57a26735c3f8ee50db
Hina0222 <hina0222@naver.com>
2024-10-01 21:35:35 +0900

    FEAT: 지도,마커 추가

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 
 const KakaoMap = () => {
-    return (
-        <div className='map-container'>
-            <div id='map' style={{width:'500px',height:'400px'}}></div>
-        </div>
-    );
+  const [location, setLocation] = useState({
+    latitude: 33.450701, 
+    longitude: 126.570667, 
+  });
+
+  const success = (position:any)=>{
+    setLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+
+  const error = () => {
+    console.log("위치 받기 실패");
+  }
+  
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  }, []);
+
+  useEffect(() => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new window.kakao.maps.LatLng(location.latitude, location.longitude),
+      level: 3,
+    };
+    const map = new window.kakao.maps.Map(container, options);
+
+    const markerPosition = new window.kakao.maps.LatLng(location.latitude, location.longitude);
+    
+    const marker = new window.kakao.maps.Marker({
+      position: markerPosition,
+    });
+    
+    marker.setMap(map);
+  }, [location]); 
+
+  return <div id="map" style={{ height: "400px" }}></div>;
 };
 
 export default KakaoMap;
