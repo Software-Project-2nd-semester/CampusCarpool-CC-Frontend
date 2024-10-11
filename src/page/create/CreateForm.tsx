36c816158c83d8be18b4b3c18a96ac41df53362d
Hina0222 <hina0222@naver.com>
2024-10-12 00:31:13 +0900

    Style : createForm 스타일 수정

--- src/page/create/CreateForm.tsx
+++ src/page/create/CreateForm.tsx
@@ -6,12 +6,17 @@ import tw from 'twin.macro';
 import { ReactComponent as Infomation } from '../../assets/create/createForm/Infomation.svg'
 import { ReactComponent as RightArrow } from '../../assets/create/createForm/RightArrow.svg'
 import { ReactComponent as Calendar } from '../../assets/create/createForm/Calendar.svg'
+import { ReactComponent as Destination } from '../../assets/create/createForm/Destination.svg'
 import MaximumPeople from './components/MaximumPeople';
 
 const SelectButton = styled(Button) <{ $isSelected: boolean }>`
     ${tw`w-1/3 text-base`}
     background-color: ${({ $isSelected }) => ($isSelected ? '#4C3EED' : '#D0D0D0')};
     color: ${({ $isSelected }) => ($isSelected ? 'white' : '#A7A7A7')};
+`;
+
+const InputButton = styled.button`
+    ${tw`flex justify-between items-center w-full`}
 `;
 
 const SubmitButton = styled(Button)`
@@ -22,38 +27,55 @@ const TextArea = styled.textarea`
     width:100%;
 `;
 
+const MtSection = styled.section`
+    ${tw`mt-8`}
+`;
+
 const CreateForm = () => {
     const location = useLocation();
 
-    const [maximumPeople,setMaximumPeople] = useState<number>(5);
+    const [maximumPeople, setMaximumPeople] = useState<number>(5);
     const [sameSex, setSameSex] = useState<boolean>(true);
     const [socialCarpoolMode, setSocialCarpoolMode] = useState<boolean>(true);
-    const [carInfo,setCarInfo] = useState<string>('');
-    const [content,setContent] = useState<string>('');
+    const [carInfo, setCarInfo] = useState<string>('');
+    const [content, setContent] = useState<string>('');
 
     const category = location.state as string;
 
     return (
         <>
-            <section className=''>
+            <section>
                 <StyledH3>출발시간</StyledH3>
-                <div className='flex justify-between items-center'>
+                <InputButton>
                     <div className='flex gap-x-3'>
-                        <p className='text-base text-gray-400'>날짜, 시간을 선택해주세요.</p>
-                        <Calendar/>
+                        <p className='text-gray-400'>날짜, 시간을 선택해주세요.</p>
+                        <Calendar />
                     </div>
-                    <RightArrow/>
-                </div>
+                    <RightArrow />
+                </InputButton>
             </section>
-            <section>
+            <MtSection>
                 <StyledH3>출발지 / 도착지</StyledH3>
-                카카오 api
-            </section>
-            <section>
+                <InputButton>
+                    <div className='flex gap-x-3'>
+                        <Destination />
+                        <p className='text-gray-400'>출발지를 입력해주세요.</p>
+                    </div>
+                    <RightArrow />
+                </InputButton>
+                <InputButton>
+                    <div className='flex gap-x-3'>
+                        <Destination />
+                        <p className='text-gray-400'>도착지를 입력해주세요.</p>
+                    </div>
+                    <RightArrow />
+                </InputButton>
+            </MtSection>
+            <MtSection>
                 <StyledH3>최대인원</StyledH3>
-                <MaximumPeople maximumPeople={maximumPeople} setMaximumPeople={setMaximumPeople}/>
-            </section>
-            <section>
+                <MaximumPeople maximumPeople={maximumPeople} setMaximumPeople={setMaximumPeople} />
+            </MtSection>
+            <MtSection>
                 <StyledH3>성별선택</StyledH3>
                 <div className='flex gap-x-3'>
                     <SelectButton
@@ -67,8 +89,8 @@ const CreateForm = () => {
                         동성만
                     </SelectButton>
                 </div>
-            </section>
-            <section>
+            </MtSection>
+            <MtSection>
                 <StyledH3>소셜카풀 여부</StyledH3>
                 <div className='flex gap-x-3'>
                     <SelectButton
@@ -86,16 +108,16 @@ const CreateForm = () => {
                     <Infomation />
                     <i className='text-sm text-gray-400'>소셜카풀 : 서로 가볍게 소통하며 가는 모드<br />조용히 : 조용히 가는 모드</i>
                 </div>
-            </section>
-            <section>
+            </MtSection>
+            <MtSection>
                 <StyledH3>차량정보</StyledH3>
-                <input type="text" value={carInfo} onChange={(e)=>{setCarInfo(e.target.value)}} placeholder="차량 정보를 입력해주세요" />
-            </section>
-            <section>
+                <input type="text" value={carInfo} onChange={(e) => { setCarInfo(e.target.value) }} placeholder="차량 정보를 입력해주세요" />
+            </MtSection>
+            <MtSection>
                 <StyledH3>이동설명</StyledH3>
-                <TextArea value={content} onChange={(e)=>{setContent(e.target.value)}}  rows={5}  placeholder="이동에 대한 설명을 적어주세요!자세히 올리면 사용자에게 도움이 됩니다!
+                <TextArea value={content} onChange={(e) => { setContent(e.target.value) }} rows={5} placeholder="이동에 대한 설명을 적어주세요!자세히 올리면 사용자에게 도움이 됩니다!
                 (예시 : 터미널에 1명 내리고 두정역으로 가요)" ></TextArea>
-            </section>
+            </MtSection>
             <Footer>
                 <SubmitButton>등록</SubmitButton>
             </Footer>
