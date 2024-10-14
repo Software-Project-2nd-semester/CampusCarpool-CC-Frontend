66fd0b7bc890ce76a204100c3601f803d19c089c
Hina0222 <hina0222@naver.com>
2024-10-15 00:51:38 +0900

    Style : createForm 수정

--- src/page/create/CreateForm.tsx
+++ src/page/create/CreateForm.tsx
@@ -11,6 +11,7 @@ import { ReactComponent as Origin } from '../../assets/create/createForm/Origin.
 import { ReactComponent as DirectionArrow } from '../../assets/create/createForm/DirectionArrow.svg'
 import MaximumPeople from './components/MaximumPeople';
 import LocationList from './components/LocationList';
+import useKakaoMapStore from "../../store/kakaoMapStore";
 
 const SelectButton = styled(Button) <{ $isSelected: boolean }>`
     ${tw`w-1/3 text-base`}
@@ -25,6 +26,9 @@ const InputButton = styled.button`
 const SubmitButton = styled(Button)`
     ${tw`text-2xl`}
 `;
+const GrayBaseP = styled.p`
+    ${tw`text-gray-400 text-base`}
+`;
 
 const TextArea = styled.textarea`
     width:100%;
@@ -37,6 +41,7 @@ const MtSection = styled.section`
 const CreateForm = () => {
     const location = useLocation();
 
+    const startEndPoint = useKakaoMapStore((state) => state.startEndPoint);
     const [maximumPeople, setMaximumPeople] = useState<number>(5);
     const [sameSex, setSameSex] = useState<boolean>(true);
     const [socialCarpoolMode, setSocialCarpoolMode] = useState<boolean>(true);
@@ -64,7 +69,7 @@ const CreateForm = () => {
                         <StyledH3>출발시간</StyledH3>
                         <InputButton>
                             <div className='flex gap-x-3'>
-                                <p className='text-gray-400 text-base'>날짜, 시간을 선택해주세요.</p>
+                                <GrayBaseP>날짜, 시간을 선택해주세요.</GrayBaseP>
                                 <Calendar />
                             </div>
                             <RightArrow />
@@ -75,16 +80,24 @@ const CreateForm = () => {
                         <StyledH3>출발지 / 도착지</StyledH3>
                         <InputButton onClick={() => { OpenLocationList("origin") }}>
                             <div className='flex gap-x-3'>
-                                <Origin />
-                                <p className='text-gray-400 text-base'>출발지를 입력해주세요.</p>
+                                <Origin color={startEndPoint.startPoint.address_name ? 'black' : '#A7A7A7'} />
+                                {startEndPoint.startPoint.address_name ?
+                                    <p className='text-base'>{startEndPoint.startPoint.address_name}</p>
+                                    :
+                                    <GrayBaseP>출발지를 입력해주세요.</GrayBaseP>
+                                }
                             </div >
                             <RightArrow />
                         </InputButton >
-                        <DirectionArrow className='my-3' />
+                        <DirectionArrow className='my-3' color={startEndPoint.startPoint.address_name && startEndPoint.endPoint.address_name ? 'black' : '#A7A7A7'} />
                         <InputButton onClick={() => { OpenLocationList("destination") }}>
                             <div className='flex gap-x-3'>
-                                <Destination />
-                                <p className='text-gray-400 text-base'>도착지를 입력해주세요.</p>
+                                <Destination color={startEndPoint.endPoint.address_name ? 'black' : '#A7A7A7'} />
+                                {startEndPoint.endPoint.address_name ?
+                                    <p className='text-base'>{startEndPoint.endPoint.address_name}</p>
+                                    :
+                                    <GrayBaseP>도착지를 입력해주세요.</GrayBaseP>
+                                }
                             </div >
                             <RightArrow />
                         </InputButton >
@@ -131,15 +144,17 @@ const CreateForm = () => {
                         </div>
                     </MtSection>
 
-                    <MtSection>
-                        <StyledH3>차량정보</StyledH3>
-                        <input type="text" value={carInfo} onChange={(e) => { setCarInfo(e.target.value) }} placeholder="차량 정보를 입력해주세요" className='text-base' />
-                    </MtSection >
+                    {category === "driver" &&
+                        <MtSection>
+                            <StyledH3>차량 정보</StyledH3>
+                            <input type="text" value={carInfo} onChange={(e) => { setCarInfo(e.target.value) }} placeholder="차량 정보를 입력해주세요" className='text-base' />
+                        </MtSection >
+                    }
 
                     <MtSection>
-                        <StyledH3>이동설명</StyledH3>
+                        <StyledH3>이동 설명</StyledH3>
                         <TextArea value={content} onChange={(e) => { setContent(e.target.value) }} rows={5} placeholder="이동에 대한 설명을 적어주세요!자세히 올리면 사용자에게 도움이 됩니다!
-                (예시 : 터미널에 1명 내리고 두정역으로 가요)" className='text-base'></TextArea>
+                        (예시 : 터미널에 1명 내리고 두정역으로 가요)" className='text-base'></TextArea>
                     </MtSection >
 
                     <Footer>
