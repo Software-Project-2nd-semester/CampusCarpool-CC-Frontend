ba321ca6e807c99af9880769adc6c81cf4f4db8a
Hina0222 <hina0222@naver.com>
2024-10-15 22:27:52 +0900

    Style : 출발지 도착지 검색 모달로 변경

--- src/page/create/components/LocationList.tsx
+++ src/page/create/components/LocationList.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import tw from "twin.macro";
-import { Button, StyledH3, Footer } from '../../../scss/styled/Common';
+import { Button, StyledH3, Footer, Header } from '../../../scss/styled/Common';
 import KakaoMap from "../../../components/KakaoMap";
+import { ReactComponent as Cancel } from '../../../assets/layout/header/Cancel.svg'
 
 interface OwnProps {
     type: string
@@ -9,8 +10,17 @@ interface OwnProps {
 }
 
 const LocationListModal = styled.div`
-    ${tw`absolute bg-white`}
-    width: calc(100% - 2.5rem);
+    ${tw`fixed bg-white w-screen h-screen left-0 top-0 py-20 px-5 z-20`}
+    
+    @media (min-width: 500px) {
+        transform: translateX(-50%);
+        left: 50%;
+        width:430px;
+    }
+`;
+
+const ModalFooter = styled(Footer)`
+    ${tw`z-20`}
 `;
 
 const SubmitButton = styled(Button)`
@@ -20,11 +30,16 @@ const SubmitButton = styled(Button)`
 const LocationList = ({ type, setOpenLocationList }: OwnProps) => {
     return (
         <LocationListModal>
+            <Header>
+                <button onClick={() => { setOpenLocationList({ isOpen: false, type: '' }); }} className='my-auto'>
+                    <Cancel />
+                </button>
+            </Header>
             <StyledH3>{type === 'origin' ? '출발지가 어디신가요?' : '목적지가 어디신가요?'}</StyledH3>
             <KakaoMap type={type} />
-            <Footer>
+            <ModalFooter>
                 <SubmitButton onClick={() => { setOpenLocationList({ isOpen: false, type: '' }); }}>확인</SubmitButton>
-            </Footer>
+            </ModalFooter>
         </LocationListModal>
     );
 };
