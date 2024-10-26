875a3c343d78a453f85f2362f40579293651b888
Hina0222 <hina0222@naver.com>
2024-10-26 20:06:42 +0900

    fix : API코드에 기능 분리

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -9,7 +9,7 @@ declare global {
 }
 
 interface OwnProps {
-  type: string
+  type: string;
 }
 
 const KakaoMap = ({ type }: OwnProps) => {
@@ -21,32 +21,81 @@ const KakaoMap = ({ type }: OwnProps) => {
   const currentMap = useKakaoMapStore((state) => state.currentMap);
   const setCurrentMap = useKakaoMapStore((state) => state.setCurrentMap);
   const startEndMarker = useKakaoMapStore((state) => state.startEndMarker);
-  const setStartEndMarker = useKakaoMapStore((state) => state.setStartEndMarker);
+  const setStartEndMarker = useKakaoMapStore(
+    (state) => state.setStartEndMarker
+  );
   const setTaxiCharge = useKakaoMapStore((state) => state.setTaxiCharge);
-
-  const [keyWordStart, setKeyWordStart] = useState<string>('');
-  const [keyWordEnd, setKeyWordEnd] = useState<string>('');
+  
+  const [keyWordStart, setKeyWordStart] = useState<string>("");
+  const [keyWordEnd, setKeyWordEnd] = useState<string>("");
   const [isOrigin, setIsOrigin] = useState<boolean>(true);
   const [placeList, setPlaceList] = useState<any[]>([]);
   const [currentPolyline, setCurrentPolyline] = useState<any>(null);
 
-  //길찾기API Get요청
+  // 현재위치 받아오기
   useEffect(() => {
-    if (startEndPoint.startPoint.x && startEndPoint.endPoint.x) {
-      if (currentPolyline) {
-        currentPolyline.setMap(null);
-      }
-
-      GetDirection(startEndPoint, currentMap).then(result => {
-        if (result) {
-          const { polyline, taxiCharge } = result;
-
-          setCurrentPolyline(polyline);
-          setTaxiCharge(taxiCharge);
-        }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error, {
+        enableHighAccuracy: true,
+        timeout: 5000,
       });
     }
+  }, []);
+
+  //길찾기API Get요청
+  useEffect(() => {
+    const FindDirection = async () => {
+      try {
+        if (startEndPoint.startPoint.x && startEndPoint.endPoint.x) {
+          const result = await GetDirection(startEndPoint);
+  
+          if (result) {
+            const duration  = result.routes[0].summary.duration;
+            const taxiCharge  = result.routes[0].summary.fare.taxi;
+
+            DrawLinePath(result);
+            setTaxiCharge(taxiCharge);
+          }
+        }
+      } catch (error) {
+        // console.error("경로 요청 중 오류 발생:", error);
+      }
+    };
+  
+    FindDirection();
   }, [startEndPoint, currentMap]);
+
+  const DrawLinePath = (data:any) => {
+    if (currentPolyline) {
+      currentPolyline.setMap(null);
+    }
+
+    const linePath: any[] = [];
+
+    data.routes[0].sections[0].roads.forEach((router: any) => {
+      router.vertexes.forEach((vertex: number, index: number) => {
+        if (index % 2 === 0) {
+          linePath.push(
+            new window.kakao.maps.LatLng(
+              router.vertexes[index + 1],
+              router.vertexes[index]
+            )
+          );
+        }
+      });
+    });
+
+    const polyline = new window.kakao.maps.Polyline({
+      path: linePath,
+      strokeWeight: 6,
+      strokeColor: "#4C3EED",
+      strokeOpacity: 0.8,
+      strokeStyle: "solid",
+    });
+
+    setCurrentPolyline(polyline);
+    polyline.setMap(currentMap);
+  };
 
   // 현재위치 받아오기 성공
   const success = (position: any) => {
@@ -61,23 +110,13 @@ const KakaoMap = ({ type }: OwnProps) => {
     console.log("위치 받기 실패");
   };
 
-  // 현재위치 받아오기
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error, {
-        enableHighAccuracy: true,
-        timeout: 5000,
-      });
-    }
-  }, []);
-
   // 검색 실행 함수
   const SearchPlaces = (keyWord: string, isOrigin: boolean) => {
     setIsOrigin(isOrigin);
 
     const ps = new window.kakao.maps.services.Places();
 
-    if (!(!keyWord || keyWord.trim() === '' || !currentMap)) {
+    if (!(!keyWord || keyWord.trim() === "" || !currentMap)) {
       ps.keywordSearch(keyWord, PlacesSearchCB);
     } else {
       alert("검색어를 입력해주세요");
@@ -89,9 +128,9 @@ const KakaoMap = ({ type }: OwnProps) => {
     if (status === window.kakao.maps.services.Status.OK) {
       DisplayPlaces(data);
     } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
-      alert('검색 결과가 존재하지 않습니다.');
+      alert("검색 결과가 존재하지 않습니다.");
     } else if (status === window.kakao.maps.services.Status.ERROR) {
-      alert('검색 결과 중 오류가 발생했습니다.');
+      alert("검색 결과 중 오류가 발생했습니다.");
     }
   };
 
@@ -104,7 +143,7 @@ const KakaoMap = ({ type }: OwnProps) => {
     const newCenter = new window.kakao.maps.LatLng(firstPlace.y, firstPlace.x);
 
     currentMap.setCenter(newCenter);
-  }
+  };
 
   const preventScroll = (e: TouchEvent) => {
     e.preventDefault();
@@ -122,17 +161,23 @@ const KakaoMap = ({ type }: OwnProps) => {
 
     currentMap.setCenter(newCenter);
     setStartEndPoint(newPoint, isOrigin);
-  }
+  };
 
   // 출발지, 도착지 아이콘 생성 및 이동
   useEffect(() => {
     const geocoder = new window.kakao.maps.services.Geocoder(); // Geocoder 객체 생성
 
     const createMarker = (point: any, isOrigin: boolean) => {
-      const markerSrc = (isOrigin ? 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/red_b.png' : 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/blue_b.png');
+      const markerSrc = isOrigin
+        ? "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/red_b.png"
+        : "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/blue_b.png";
       const markerSize = new window.kakao.maps.Size(50, 45);
       const markerOption = { offset: new window.kakao.maps.Point(15, 43) };
-      const markerImage = new window.kakao.maps.MarkerImage(markerSrc, markerSize, markerOption);
+      const markerImage = new window.kakao.maps.MarkerImage(
+        markerSrc,
+        markerSize,
+        markerOption
+      );
 
       const markerPosition = new window.kakao.maps.LatLng(point.y, point.x);
 
@@ -144,25 +189,29 @@ const KakaoMap = ({ type }: OwnProps) => {
       });
 
       // 드래그 끝났을 때 이벤트 처리
-      window.kakao.maps.event.addListener(newMarker, 'dragend', function () {
-        const latlng = newMarker.getPosition();  // 마커의 새 좌표 가져오기
+      window.kakao.maps.event.addListener(newMarker, "dragend", function () {
+        const latlng = newMarker.getPosition(); // 마커의 새 좌표 가져오기
         // 좌표 -> 주소 변환
-        geocoder.coord2Address(latlng.getLng(), latlng.getLat(), function (result: any, status: any) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            const address = result[0].address.address_name;
-            const newPoint = {
-              address_name: address, // 변환한 주소를 저장
-              x: latlng.getLng(),
-              y: latlng.getLat(),
+        geocoder.coord2Address(
+          latlng.getLng(),
+          latlng.getLat(),
+          function (result: any, status: any) {
+            if (status === window.kakao.maps.services.Status.OK) {
+              const address = result[0].address.address_name;
+              const newPoint = {
+                address_name: address, // 변환한 주소를 저장
+                x: latlng.getLng(),
+                y: latlng.getLat(),
+              };
+              // startEndPoint에 새로운 좌표 및 주소 업데이트
+              setStartEndPoint(newPoint, isOrigin);
             }
-            // startEndPoint에 새로운 좌표 및 주소 업데이트
-            setStartEndPoint(newPoint, isOrigin);
           }
-        });
+        );
       });
 
       return newMarker;
-    }
+    };
 
     // 출발지 마커 생성 및 업데이트
     if (startEndPoint.startPoint) {
@@ -188,7 +237,10 @@ const KakaoMap = ({ type }: OwnProps) => {
   useEffect(() => {
     const container = document.getElementById("map");
     const options = {
-      center: new window.kakao.maps.LatLng(location.latitude, location.longitude),
+      center: new window.kakao.maps.LatLng(
+        location.latitude,
+        location.longitude
+      ),
       level: 3,
       draggable: true,
     };
@@ -196,25 +248,27 @@ const KakaoMap = ({ type }: OwnProps) => {
     setCurrentMap(map);
 
     // 현재 위치 마커
-    const markerPosition = new window.kakao.maps.LatLng(location.latitude, location.longitude);
+    const markerPosition = new window.kakao.maps.LatLng(
+      location.latitude,
+      location.longitude
+    );
     const marker = new window.kakao.maps.Marker({
       position: markerPosition,
     });
 
     marker.setMap(map);
 
-    container?.addEventListener('touchmove', preventScroll);
+    container?.addEventListener("touchmove", preventScroll);
 
     return () => {
-      container?.removeEventListener('touchmove', preventScroll);
+      container?.removeEventListener("touchmove", preventScroll);
     };
-
   }, [location]);
 
   return (
     <div>
       <div id="map" style={{ height: "400px" }}></div>
-      {type === 'origin' ?
+      {type === "origin" ? (
         <>
           <input
             type="text"
@@ -222,9 +276,15 @@ const KakaoMap = ({ type }: OwnProps) => {
             onChange={(e) => setKeyWordStart(e.target.value)}
             placeholder="출발지 검색"
           />
-          <button onClick={() => { SearchPlaces(keyWordStart, true) }}>검색하기</button>
+          <button
+            onClick={() => {
+              SearchPlaces(keyWordStart, true);
+            }}
+          >
+            검색하기
+          </button>
         </>
-        :
+      ) : (
         <>
           <input
             type="text"
@@ -232,12 +292,23 @@ const KakaoMap = ({ type }: OwnProps) => {
             onChange={(e) => setKeyWordEnd(e.target.value)}
             placeholder="도착지 검색"
           />
-          <button onClick={() => { SearchPlaces(keyWordEnd, false) }}>검색하기</button>
+          <button
+            onClick={() => {
+              SearchPlaces(keyWordEnd, false);
+            }}
+          >
+            검색하기
+          </button>
         </>
-      }
+      )}
       <ul>
         {placeList.map((place, idx) => (
-          <li key={idx} onClick={() => { UpdateStartEndPoint(place) }}>
+          <li
+            key={idx}
+            onClick={() => {
+              UpdateStartEndPoint(place);
+            }}
+          >
             {place.place_name}
           </li>
         ))}
@@ -246,4 +317,4 @@ const KakaoMap = ({ type }: OwnProps) => {
   );
 };
 
-export default KakaoMap;  
+export default KakaoMap;
