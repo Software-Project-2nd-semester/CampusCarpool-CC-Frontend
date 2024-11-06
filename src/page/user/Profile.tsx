e25a1ad5a56b6e3316c57607efc10b4e7880560e
chahyunwoo00 <ckgusdn8@naver.com>
2024-11-07 02:46:09 +0900

    회원정보 put

--- src/page/user/Profile.tsx
+++ src/page/user/Profile.tsx
@@ -3,39 +3,120 @@ import { useEffect, useState } from 'react';
 import ProfileImage from '../../assets/user/profile.svg';
 import { useNavigate } from 'react-router-dom';
 import user from '../../store/userStore/user';
+import axios from 'axios';
 
 const genders=['남성','여성']
 const ages=['20대','30대','40대','50대','60대','70대','80대','90대']
+const genderToString = (gender:any) => {
+  if (gender === false) {
+    return '남성';
+  } else if (gender === true) {
+    return '여성';
+  } 
+};
+const genderToBoolean = (gender:any) => {
+  if (gender === '남성') {
+    return false;
+  } else if (gender === '여성') {
+    return true;
+  }
+};
+function ageToEnglish(age:any) {
+  if (age === '20대') {
+    return 'TWENTY';
+  } else if (age === '30대') {
+    return 'THIRTY';
+  } else if (age === '40대') {
+    return 'FORTY';
+  } else if (age === '50대') {
+    return 'FIFTY';
+  } else if (age === '60대') {
+    return 'SIXTY';
+  } else if (age === '70대') {
+    return 'SEVENTY';
+  } else if (age === '80대') {
+    return 'EIGHTY';
+  } else if (age === '90대') {
+    return 'NINETY';
+  }
+}
+
 
 const Profile = () => {
  const navigate=useNavigate()
  const { name,nickname,age,gender,intro,setName,setNickname,setAge,setGender,setIntro} = user();
+ const [name_, setName_] = useState(name);
+ const [nickname_,setNickname_]=useState(nickname)
+ const [age_,setAge_]=useState(age)
+ const [gender_,setGender_]=useState(gender)
+ const [intro_,setIntro_]=useState(intro)
+ //console.log(name,nickname,age,gender,intro)
+ console.log(name_,nickname_,age_,gender_,intro_)
+ const handleClick = async (event:any) => {
+  event.preventDefault();   
+  try {
+    const response = await axios.put(
+      `${process.env.REACT_APP_API_URL}/api/user/sub`, // URL에 환경변수 사용
+      {
+        "name": name_,
+        "nickname": nickname_,
+        "age": age_,
+        "intro": intro_
+      }, 
+      {
+        headers: {
+          'sub': sessionStorage.getItem('sub') // sessionStorage에서 'sub' 값을 가져옴
+        }
+      }
+    );
+    setName(name_)
+    setNickname(nickname_)
+    setAge(age_)
+    setIntro(intro_)
+    navigate('/user');
+  } catch (error) {
+    console.error('에러 발생:', error);
+  }
+};
 
- console.log(name,nickname,age,gender,intro)
- 
-  return (
+
+ return (
     <section>
-      <form className='flex flex-col gap-8' onSubmit={()=>{
-        navigate('/user')
-      }}>
+      <form className='flex flex-col gap-8' onSubmit={handleClick}>
         <div>
           <img style={{width:'100px',height:'100px',margin:'0 auto'}}src={ProfileImage} alt='alt'></img>
           <p className='text-center font-bold'>프로필 설정</p>
         </div>
         <div>
           <p>이름</p>
-          <input className='pl-2 border border-solid border-black rounded h-8 w-full' type='text'></input>
+          <input value={name_}
+          className='pl-2 border border-solid border-black rounded h-8 w-full'
+          onChange={(e) => {setName_(e.target.value)}}
+          type='text'></input>
         </div>
         <div>
           <p>닉네임</p>
-          <input className='pl-2 border border-solid border-black rounded h-8 w-full' type='text'></input>
+          <input value={nickname_}
+          onChange={(e)=>{setNickname_(e.target.value)}}
+          className='pl-2 border border-solid border-black rounded h-8 w-full' type='text'></input>
         </div>
         <div>
           <p>성별</p>
           <div className='flex gap-4'>
-            {genders.map((gender)=>{
+            {genders.map((gen,index)=>{
+              let classname;
+              const btn1='px-8 py-2 bg-[#4C3EED] text-white rounded'
+              const btn2='px-8 py-2 bg-[#D0D0D0] text-[#A7A7A7] rounded'
+              if(gen===genderToString(gender_)){
+                classname=btn1
+              }else{
+                classname=btn2
+              }
               return(
-                <button type='button' className='px-8 py-2 bg-[#D0D0D0] text-[#A7A7A7] rounded'>{gender}</button>
+
+                <button 
+                  onClick={()=>{setGender_(genderToBoolean(gen))}}
+                key={index} type='button' className={classname}>{gen}</button>
               )
             })}
           </div>
@@ -43,12 +124,30 @@ const Profile = () => {
         <div>
           <p>나이</p>
           <div className='flex flex-wrap gap-2'>
-            {ages.map((age)=>{
+            {ages.map((a,index)=>{
+              let classname;
+              const btn1='px-8 py-2 bg-[#4C3EED] text-white rounded'
+              const btn2='px-8 py-2 bg-[#D0D0D0] text-[#A7A7A7] rounded'
+              if(ageToEnglish(a)===age_){
+                classname=btn1
+              }else{
+                classname=btn2
+              }
               return(
-                <button type='button' className='px-8 py-2 bg-[#D0D0D0] text-[#A7A7A7] rounded' >{age}</button>
+                <button
+                  onClick={()=>{setAge_(ageToEnglish(a))}}
+                key={index} type='button' className={classname} >{a}</button>
               )
             })}
           </div>
+        </div>
+        <div>
+            <p>한줄 소개</p>
+            <textarea 
+            value={intro_}
+            className='border-2 w-full h-20 border-black border-solid rounded'
+            onChange={(e)=>{setIntro_(e.target.value)}}>
+            </textarea>
         </div>
         {/* <input type="file" /> */}
         <div className='w-screen h-16 px-8 py-2 fixed bottom-0 left-0 border-solid border-t-2 border-black'>
