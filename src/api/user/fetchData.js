5a78496863be3c7b34b1720e3bdac3ab26ab7a6c
chahyunwoo00 <ckgusdn8@naver.com>
2024-11-11 21:45:19 +0900

    feat: env적용

--- src/api/user/fetchData.js
+++ src/api/user/fetchData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get('http://ec2-15-164-222-34.ap-northeast-2.compute.amazonaws.com:8080/api/articles/sub',
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/articles/sub`,
       {
         headers: {
           sub: sessionStorage.getItem('sub')
